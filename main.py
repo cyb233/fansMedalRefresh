7e51257eda39b755be30ba7f46f0ce1d4fa226ac
cyb233 <github@shuvi.moe>
2025-10-07 01:46:08 +0800

    转义 Markdown 特殊字符

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __VERSION__ = "0.0.1"
+import re
 from loguru import logger
 
 import asyncio
@@ -27,7 +28,7 @@ logger.add(
     diagnose=True,
     level=config.log.level.upper(),
 )
-log = logger.bind(user="B站粉丝牌助手")
+log = logger.bind(user="B站粉丝牌点亮助手")
 # 添加日志文件
 if config.log.enabled:
     log_file = os.path.join(os.path.dirname(__file__), config.log.file)
@@ -40,6 +41,14 @@ if config.log.enabled:
         retention="30 days",
         level=config.log.level.upper(),
     )
+
+
+def escape_markdown(text: str) -> str:
+    """
+    转义 Markdown 特殊字符
+    """
+    special_chars = r"_*[]()~`>#+-=|{}.!"
+    return re.sub(f"([{re.escape(special_chars)}])", r"\\\1", text)
 
 
 @log.catch
@@ -58,24 +67,33 @@ async def main():
     except Exception as e:
         log.exception(e)
         messageList.append(f"用户执行失败: {e}")
-    title = "点亮B站粉丝牌 执行结果推送"
-    content = "\n\n".join("\n".join(row) for row in messageList)
-    titlemd = f"*{title}*"
-    contentmd = "\n\n".join(
-        "_{}_".format(row[0]) + "".join(f"\n> {line}" for line in row[1:])
-        for row in messageList
-        if row
-    )
+    title_raw = "B站粉丝牌点亮助手-执行结果推送"
+    content_raw = "\n\n".join("\n".join(row) for row in messageList)
     # 推送
-    log.info(f"推送内容：\n{title}\n\n{content}")
+    log.info(f"推送内容：\n{title_raw}\n\n{content_raw}")
     for push_cfg in config.push:
         notifier = get_notifier(push_cfg.provider_name)
         if notifier:
+            title = (
+                f"*{escape_markdown(title_raw)}*"
+                if push_cfg.use_markdown
+                else title_raw
+            )
+            content = (
+                "\n\n".join(
+                    "_{}_".format(escape_markdown(row[0]))
+                    + "".join(f"\n> {escape_markdown(line)}" for line in row[1:])
+                    for row in messageList
+                    if row
+                )
+                if push_cfg.use_markdown
+                else content_raw
+            )
             res: Response = notifier.notify(
                 proxies=push_cfg.proxies,
                 **push_cfg._config,
-                title=titlemd if push_cfg.use_markdown else title,
-                content=contentmd if push_cfg.use_markdown else content,
+                title=title,
+                content=content,
             )
             log.info(f"使用 {push_cfg.provider_name} 推送结果: {res}")
             if not res.ok:
